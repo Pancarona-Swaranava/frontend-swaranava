2b7f3d9602c95fbc93b946070e04194271a44673
SWARANAVA Developer <user@swaranava.com>
2025-11-23 02:15:51 -0800

    Add navbar and footer components with custom fonts and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import localFont from "next/font/local";
 import "./globals.css";
+
+const flatory = localFont({
+  src: "../../public/font/flatory-serif-condensed.ttf",
+  variable: "--font-flatory",
+  display: "swap",
+});
+
+const glacial = localFont({
+  src: [
+    {
+      path: "../../public/font/glacial-indifference.regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/font/glacial-indifference.bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-glacial",
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${flatory.variable} ${glacial.variable}`}>
       <body>
         {children}
       </body>
